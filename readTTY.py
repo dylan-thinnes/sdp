dd42ec515daa4c719cf3c4ae7f961d5a3e2f2ccf
Dylan Thinnes <s1737075@jingxi.inf.ed.ac.uk>
2020-02-24 13:23:44 +0000

    Add getTTY / readTTY scripts

---
+++ readTTY.py
@@ -0,0 +1,23 @@
+import serial
+import time
+import sys
+
+ser = serial.Serial(sys.argv[0], 9600, timeout=1)  
+
+# send start sign to Arduino
+for i in range(1, 4):
+    ser.write('S'.encode())
+    time.sleep(1)
+
+# run main task
+try:
+    while True:
+        hear = str(ser.readline().decode())
+        if hear.startswith('K'):  # when the head of received data is 'K', read frs and sec
+            fst = int(hear[1:5])  # from 2nd to 4th is first value
+            snd = int(hear[5:9])  # from 5th to 8th is second value
+            print("%i %i %f" % (fst, snd, time.time())
+
+except KeyboardInterrupt:
+    print("Can't read TTY.")
+    ser.close()

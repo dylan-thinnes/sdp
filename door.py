123e2d2207b62774d6b7f379cf5dfb2da69d4ff9
hfzcjdhr <60181657+hfzcjdhr@users.noreply.github.com>
2020-03-13 15:03:12 +0000

    Update door.py

--- door.py
+++ door.py
@@ -1,39 +1,43 @@
 import RPi.GPIO as GPIO
+import time
 
 door_state = "unknown"
 
 # define ports used for receiving data
-up_port = 23
-down_port = 21
+port = 23
 
 def write_state():
-        with open("door_state", "w") as  file:
-                global door_state
-                file.write(door_state)
+	with open("system_state_files/door_state", "w") as  file:
+		global door_state
+		file.write(door_state)
 
 
 def writer(func):
-        def wrapper(*args, **kwargs):
-                func(*args, **kwargs)
-                write_state()
-        return wrapper
-        @writer
-def button_down_callback(channel):
-        global door_state
-        door_state = "closed"
+	def wrapper(*args, **kwargs):
+		func(*args, **kwargs)
+		write_state()
+	return wrapper
 
 @writer
-def button_up_callback(channel):
-        global door_state
-        door_state = "open"
+def button_callback(channel):
+	global door_state
+	if GPIO.input(port):
+		door_state = "closed"
+	else:
+		door_state = "open"
+
+
 # setup the ports
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(up_port, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(down_port, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(port, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 # setup callbacks for rising falling -- door opening closing events
-GPIO.add_event_detect(up_port, GPIO.FALLING, callback=button_up_callback)
-GPIO.add_event_detect(down_port, GPIO.RISING, callback=button_down_callback)
-input("press enter to quit")
-GPIO.cleanup()
+GPIO.add_event_detect(port, GPIO.BOTH, callback=button_callback)
+
+try:
+	while True:
+		time.sleep(0.1)
+finally:
+	GPIO.cleanup()
